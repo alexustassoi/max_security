fbafbce8cef69b7621b886b87a8e1f0fff903a61
alexustassoi <alexustassoi@gmail.com>
2024-06-06 09:43:46 +0300

    fix for popup windows

--- wp-content/themes/wp-rock/footer.php
+++ wp-content/themes/wp-rock/footer.php
@@ -26,10 +26,10 @@ echo esc_html( get_template_part( 'src/template-parts/custom', 'footer' ) );
 do_action( 'wp_rock_after_site_page_tag' );
 wp_footer();
 do_action( 'wp_rock_before_body_closing_tag' );
-?>
+
 
 $popup_content      = get_field_value($global_options, 'popup_content');
 echo $popup_content ? do_shortcode($popup_content) : '';
-
+?>
 </body>
 </html>
