2cda2dc1934ff421ebd020b9a2982eb406a5ae7b
alexustassoi <alexustassoi@gmail.com>
2024-06-07 18:20:12 +0300

    Added compiled files for new blocks

---
+++ wp-content/themes/wp-rock/assets/public/js/js-block-faq.js
@@ -0,0 +1,31 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+var _a;
+var handleFaqItem = function handleFaqItem() {
+  document.body.addEventListener('click', function (e) {
+    var target = e.target;
+    var role = target.dataset.role;
+    if (!role) return;
+    switch (role) {
+      case 'handle-faq-item':
+        {
+          e.preventDefault();
+          var parentFaqItem = target.closest('.js-faq-item');
+          if (!parentFaqItem) return;
+          var actionType = parentFaqItem.classList.contains('active') ? 'remove' : 'add';
+          parentFaqItem.classList[actionType]('active');
+          break;
+        }
+      default:
+        break;
+    }
+  });
+};
+document.addEventListener('DOMContentLoaded', handleFaqItem, false);
+if (window['acf']) {
+  (_a = window['acf']) === null || _a === void 0 ? void 0 : _a.addAction('render_block_preview', handleFaqItem);
+}
+
+/******/ })()
+;
