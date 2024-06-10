80284159a759f0e21aad109943d5555300f4d82b
alexustassoi <alexustassoi@gmail.com>
2024-06-10 09:25:30 +0300

    Compiled files

--- wp-content/themes/wp-rock/functions.php
+++ wp-content/themes/wp-rock/functions.php
@@ -34,4 +34,4 @@ require THEME_DIR . '/src/inc/class-mobile-detect.php';
 
 
 /* Disable WordPress Admin Bar for all users */
-add_filter( 'show_admin_bar', '__return_false' );
+// add_filter( 'show_admin_bar', '__return_false' );
