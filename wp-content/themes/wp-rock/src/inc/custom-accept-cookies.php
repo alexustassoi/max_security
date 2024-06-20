05f393d53ef031f45e29dc147a2f74c4a92f6be3
alexustassoi <alexustassoi@gmail.com>
2024-06-20 16:44:28 +0300

    text repeater fix, cookie fix

--- wp-content/themes/wp-rock/src/inc/custom-accept-cookies.php
+++ wp-content/themes/wp-rock/src/inc/custom-accept-cookies.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Custom accept cookie functionality
  *
@@ -16,192 +17,211 @@ add_action(
         if (empty($text) || empty($btn_accept)) {
             return '';
         }
-        ?>
-        <style>
+?>
+    <style>
+        .accept-cookie-box {
+            z-index: 2000;
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            padding: 40px 0;
+            transform: translateY(100%);
+            background: #5C5D72;
+            mix-blend-mode: normal;
+        }
+
+        @media screen and (max-width: 479px) {
             .accept-cookie-box {
-                z-index: 2000;
-                position: fixed;
-                bottom: 0;
-                left: 0;
-                width: 100%;
-                padding: 40px 0;
-                transform: translateY(100%);
-                background: #5C5D72;
-                mix-blend-mode: normal;
+                padding: 16px 24px;
             }
+        }
 
-            @media screen and (max-width: 479px) {
-                .accept-cookie-box {
-                    padding: 16px 24px;
-                }
-            }
+        .accept-cookie-box.opened {
+            transform: translateY(0);
+        }
 
-            .accept-cookie-box.opened {
-                transform: translateY(0);
-            }
+        .accept-cookie-box__inner {
+            display: flex;
+            align-items: flex-start;
+            justify-content: flex-start;
+            flex-direction: column;
+        }
 
+        @media screen and (max-width: 816px) {
             .accept-cookie-box__inner {
-                display: flex;
+                flex-wrap: wrap;
                 align-items: flex-start;
                 justify-content: flex-start;
-                flex-direction: column;
             }
+        }
 
-            @media screen and (max-width: 816px) {
-                .accept-cookie-box__inner {
-                    flex-wrap: wrap;
-                    align-items: flex-start;
-                    justify-content: flex-start;
-                }
+        .accept-cookie-box__text {
+            font-family: Turbine, sans-serif;
+            font-size: 20px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 1.4;
+            color: #FFFFFF;
+            position: relative;
+            margin-bottom: 16px;
+        }
+
+        .accept-cookie-box__text p {
+            font-family: inherit;
+            font-size: inherit;
+            font-style: inherit;
+            font-weight: inherit;
+            line-height: inherit;
+            color: inherit;
+            margin-bottom: 10px;
+
+            &:last-child {
+                margin-bottom: 0;
             }
+        }
 
+        .accept-cookie-box__text h4,
+        .accept-cookie-box__text h3,
+        .accept-cookie-box__text h5 {
+            font-family: Turbine, sans-serif;
+            font-size: 30px;
+            color: #fff;
+            font-style: normal;
+            font-weight: 700;
+            line-height: 1.26;
+            margin: 0 0 16px;
+            padding: 0;
+        }
+
+        .accept-cookie-box__buttons {
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            gap: 16px;
+        }
+
+        .btn-accept,
+        .btn-reject {
+            appearance: none;
+            height: 30px;
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            padding: 0 8px;
+            border-radius: 0;
+            border: 1px solid var(--Orange, #CC7510);
+            color: #FFF;
+            font-family: Turbine, sans-serif;
+            font-size: 24px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 1.25;
+            cursor: pointer;
+        }
+
+        .btn-accept {
+            color: #fff;
+            background-color: #CC7510;
+        }
+
+        @media screen and (max-width: 768px) {
             .accept-cookie-box__text {
-                font-family: Turbine, sans-serif;
-                font-size: 20px;
-                font-style: normal;
-                font-weight: 400;
-                line-height: 1.4;
-                color: #FFFFFF;
-                position: relative;
-                margin-bottom: 16px;
+                font-size: 18px;
             }
 
-            .accept-cookie-box__text p {
-                font-family: inherit;
-                font-size: inherit;
-                font-style: inherit;
-                font-weight: inherit;
-                line-height: inherit;
-                color: inherit;
-                margin-bottom: 10px;
-
-                &:last-child {
-                    margin-bottom: 0;
-                }
-            }
             .accept-cookie-box__text h4,
             .accept-cookie-box__text h3,
             .accept-cookie-box__text h5 {
-                font-family: Turbine, sans-serif;
-                font-size: 30px;
-                color: #fff;
-                font-style: normal;
-                font-weight: 700;
-                line-height: 1.26;
-                margin: 0 0 16px;
-                padding: 0;
-            }
-
-            .accept-cookie-box__buttons {
-                display: flex;
-                align-items: center;
-                justify-content: flex-start;
-                gap: 16px;
+                font-size: 24px;
             }
 
             .btn-accept,
             .btn-reject {
-                appearance: none;
-                height: 30px;
-                display: inline-flex;
-                align-items: center;
-                justify-content: center;
-                padding: 0 8px;
-                border-radius: 0;
-                border: 1px solid var(--Orange, #CC7510);
-                color: #FFF;
-                font-family: Turbine, sans-serif;
-                font-size: 24px;
-                font-style: normal;
-                font-weight: 400;
-                line-height: 1.25;
-                cursor: pointer;
+                font-size: 16px;
+                height: 24px;
             }
+        }
 
-            .btn-accept {
-                color: #fff;
-                background-color: #CC7510;
-            }
+        @media screen and (max-width: 479px) {}
+    </style>
 
-            @media screen and (max-width: 768px) {
-                .accept-cookie-box__text {
-                    font-size: 18px;
-                }
-                .accept-cookie-box__text h4,
-                .accept-cookie-box__text h3,
-                .accept-cookie-box__text h5 {
-                    font-size: 24px;
-                }
-
-                .btn-accept,
-                .btn-reject {
-                    font-size: 16px;
-                    height: 24px;
-                }
-            }
-
-            @media screen and (max-width: 479px) {
-
-            }
-        </style>
-
-        <div class="accept-cookie-box js-accept-cookie-box">
-            <div class="custom-container">
-                <div class="accept-cookie-box__inner">
-                    <div class="accept-cookie-box__text">
+    <div class="accept-cookie-box js-accept-cookie-box">
+        <div class="custom-container">
+            <div class="accept-cookie-box__inner">
+                <div class="accept-cookie-box__text">
+                    <?php
+                    echo do_shortcode($text);
+                    ?>
+                </div>
+                <div class="accept-cookie-box__buttons">
+                    <button class="accept-cookie-box__reject-btn  btn-reject js-reject-cookie-btn" data-role="reject-cookie">
                         <?php
-                        echo do_shortcode($text);
+                        echo do_shortcode($btn_reject);
                         ?>
-                    </div>
-                    <div class="accept-cookie-box__buttons">
-                        <button class="accept-cookie-box__reject-btn  btn-reject js-reject-cookie-btn"
-                                data-role="reject-cookie">
-                            <?php
-                            echo do_shortcode($btn_reject);
-                            ?>
-                        </button>
-                        <button class="accept-cookie-box__accept-btn  btn-accept js-accept-cookie-btn"
-                                data-role="accept-cookie">
-                            <?php
-                            echo do_shortcode($btn_accept);
-                            ?>
-                        </button>
-                    </div>
+                    </button>
+                    <button class="accept-cookie-box__accept-btn  btn-accept js-accept-cookie-btn" data-role="accept-cookie">
+                        <?php
+                        echo do_shortcode($btn_accept);
+                        ?>
+                    </button>
                 </div>
             </div>
         </div>
+    </div>
 
-        <script>
+    <script>
+        window.addEventListener('load', (event) => {
+            const ACCEPT_COOKIE_BOX = document.querySelector('.js-accept-cookie-box');
+            const CHAT_ICON = document.querySelector('.chat-icon');
+            const ACCEPT_COOKIE_CHECK = getCookie('accept-cookie');
 
-            window.addEventListener('load', (event) => {
-                const ACCEPT_COOKIE_BOX   = document.querySelector('.js-accept-cookie-box');
-                const CHAT_ICON           = document.querySelector('.chat-icon');
-                const ACCEPT_COOKIE_CHECK = localStorage.getItem('accept-cookie');
+            if (!ACCEPT_COOKIE_CHECK || +ACCEPT_COOKIE_CHECK !== 1) {
+                ACCEPT_COOKIE_BOX && ACCEPT_COOKIE_BOX.classList.add('opened');
+                document.body.classList.add('accept-cookie-box-is-opened');
+            }
 
-                if ( !ACCEPT_COOKIE_CHECK || +ACCEPT_COOKIE_CHECK !== 1 ) {
-                    (ACCEPT_COOKIE_BOX) && ACCEPT_COOKIE_BOX.classList.add('opened');
-                    document.body.classList.add('accept-cookie-box-is-opened');
+            document.body.addEventListener('click', function(event) {
+                const ROLE = event.target.dataset.role;
+                if (!ROLE) return;
+
+                switch (ROLE) {
+                    case 'accept-cookie':
+                        setCookie('accept-cookie', 1, 30);
+                        ACCEPT_COOKIE_BOX && ACCEPT_COOKIE_BOX.classList.remove('opened');
+                        document.body.classList.remove('accept-cookie-box-is-opened');
+                        break;
+                    case 'reject-cookie':
+                        setCookie('accept-cookie', 1, 7);
+                        ACCEPT_COOKIE_BOX && ACCEPT_COOKIE_BOX.classList.remove('opened');
+                        document.body.classList.remove('accept-cookie-box-is-opened');
+                        break;
                 }
-
-                document.body.addEventListener('click', function(event) {
-                    const ROLE = event.target.dataset.role;
-                    if ( !ROLE ) return;
-
-                    switch ( ROLE ) {
-                        case 'accept-cookie':
-                            localStorage.setItem('accept-cookie', 1);
-                            (ACCEPT_COOKIE_BOX) && ACCEPT_COOKIE_BOX.classList.remove('opened');
-                            document.body.classList.remove('accept-cookie-box-is-opened');
-                            break;
-                        case 'reject-cookie':
-                            (ACCEPT_COOKIE_BOX) && ACCEPT_COOKIE_BOX.classList.remove('opened');
-                            document.body.classList.remove('accept-cookie-box-is-opened');
-                            break;
-                    }
-                });
             });
+        });
 
-        </script>
-        <?php
+        function setCookie(name, value, days) {
+            const d = new Date();
+            d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+            const expires = "expires=" + d.toUTCString();
+            document.cookie = name + "=" + value + ";" + expires + ";path=/";
+        }
+
+        function getCookie(name) {
+            const nameEQ = name + "=";
+            const ca = document.cookie.split(';');
+            for (let i = 0; i < ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) === ' ') {
+                    c = c.substring(1, c.length);
+                }
+                if (c.indexOf(nameEQ) === 0) {
+                    return c.substring(nameEQ.length, c.length);
+                }
+            }
+            return null;
+        }
+    </script>
+<?php
     }
 );
