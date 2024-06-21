db42df02459212b7b8f1de1f851ad419f0623c3d
alexustassoi <alexustassoi@gmail.com>
2024-06-21 16:08:10 +0300

    Compiled files

--- wp-content/themes/wp-rock/assets/public/js/block-custom-content.js
+++ wp-content/themes/wp-rock/assets/public/js/block-custom-content.js
@@ -1,28 +1,7 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	!function() {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = function(exports) {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	}();
-/******/ 	
-/************************************************************************/
 var __webpack_exports__ = {};
-/*!*******************************************************!*\
-  !*** ./src/scss/acf-blocks/block-custom-content.scss ***!
-  \*******************************************************/
-__webpack_require__.r(__webpack_exports__);
 // extracted by mini-css-extract-plugin
 
 /******/ })()
 ;
-//# sourceMappingURL=block-custom-content.js.map
