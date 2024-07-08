a84195eb2d396748d6836e4ce97c83a837a5a79e
alexustassoi <alexustassoi@gmail.com>
2024-07-08 10:18:44 +0300

    Added "fontsize" selection

--- wp-content/themes/wp-rock/src/inc/custom-hooks.php
+++ wp-content/themes/wp-rock/src/inc/custom-hooks.php
@@ -594,3 +594,30 @@ function wp_rock_block_space_class($is_block_space, $key) : string
     return $block_space_class;
 }
 
+
+
+/**
+ * Add Formats to TinyMCE
+ * - https://developer.wordpress.org/reference/hooks/tiny_mce_before_init/
+ * - https://codex.wordpress.org/Plugin_API/Filter_Reference/tiny_mce_before_init
+ *
+ * @param array $args - Arguments used to initialize the tinyMCE
+ *
+ * @return array $args  - Modified arguments
+ */
+function prefix_tinymce_toolbar($args ): array
+{
+
+    $args['fontsize_formats'] = "8px 9px 10px 11px 12px 13px 14px 15px 16px 17px 18px 19px 20px 21px 22px 23px 24px 25px 26px 27px 28px 29px 30px 31px 32px 33px 34px 35px 36px 37px 38px 39px 40px 41px 42px 43px 44px 45px 46px 47px 48px 49px 50px 51px 52px 53px 54px 55px 56px 57px 58px 59px 60px 61px 62px 63px 64px 65px 66px 67px 68px 69px 70px 71px 72px 73px 74px 75px 76px 77px 78px 79px 80px 81px 82px 83px 84px 85px 86px 87px 88px 89px 90px 91px 92px 93px 94px 95px 96px 97px 98px 99px 100px";
+
+    return $args;
+
+}
+add_filter( 'tiny_mce_before_init', 'prefix_tinymce_toolbar' );
+
+
+function my_mce_buttons_2($buttons) {
+    array_unshift($buttons, 'fontsizeselect');
+    return $buttons;
+}
+add_filter('mce_buttons_2', 'my_mce_buttons_2');
