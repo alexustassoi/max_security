1dcf8b599668a117f459f06e9a39036abfae0f20
alexustassoi <alexustassoi@gmail.com>
2024-07-16 19:29:10 +0300

    Fixed body class fields got pages

--- wp-content/themes/wp-rock/assets/public/js/js-block-our-team.js
+++ wp-content/themes/wp-rock/assets/public/js/js-block-our-team.js
@@ -1,26 +1,5 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	!function() {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = function(exports) {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	}();
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
-/*!*********************************************!*\
-  !*** ./src/js/acf-blocks/block-our-team.ts ***!
-  \*********************************************/
-__webpack_require__.r(__webpack_exports__);
 var _a;
 var initBlockOurTeam = function initBlockOurTeam() {
   var memberItem = document.querySelectorAll('.js-member-item');
@@ -55,4 +34,3 @@ if (window['acf']) {
 
 /******/ })()
 ;
-//# sourceMappingURL=js-block-our-team.js.map
--- wp-content/themes/wp-rock/assets/public/js/js-block-text-repeater.js
+++ wp-content/themes/wp-rock/assets/public/js/js-block-text-repeater.js
@@ -1,26 +1,5 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	!function() {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = function(exports) {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	}();
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
-/*!**************************************************!*\
-  !*** ./src/js/acf-blocks/block-text-repeater.ts ***!
-  \**************************************************/
-__webpack_require__.r(__webpack_exports__);
 var _a;
 var initBlockBenefts = function initBlockBenefts() {
   var beneftsTitle = document.querySelectorAll('.js-benefts-title');
@@ -44,4 +23,3 @@ if (window['acf']) {
 
 /******/ })()
 ;
-//# sourceMappingURL=js-block-text-repeater.js.map
