06c170aabae6f0d69f31ce7c26c425bd38128175
alexustassoi <alexustassoi@gmail.com>
2024-08-06 18:25:57 +0300

    Fixed contact accordion

--- wp-content/themes/wp-rock/assets/public/js/js-block-text-repeater.js
+++ wp-content/themes/wp-rock/assets/public/js/js-block-text-repeater.js
@@ -1,6 +1,5 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
-var __webpack_exports__ = {};
 var _a;
 var initBlockBenefts = function initBlockBenefts() {
   var beneftsTitle = document.querySelectorAll('.js-benefts-title');
